b378324f3db141efb1c35169a9b9a5a4e222993e
Kevin Simms <ksimms2@cnm.edu>
2019-04-17 15:08:17 -0600

    Initial commit

---
+++ src/GFG.java
@@ -0,0 +1,25 @@
+public class GFG {
+
+  static boolean isPerfectSquare(double x)
+  {
+
+    // Find floating point value of
+    // square root of x.
+    double sr = Math.sqrt(x);
+
+    // If square root is an integer
+    return ((sr - Math.floor(sr)) == 0);
+  }
+
+  // Driver code
+  public static void main(String[] args)
+  {
+    double x = 2500;
+
+    if (isPerfectSquare(x))
+      System.out.print("Yes");
+    else
+      System.out.print("No");
+  }
+}
+
